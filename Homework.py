b09f9152cb516a83d7911a5a67ee864660e62c89
rlauture1 <rlauture1@babson.edu>
2016-09-06 21:23:54 -0400

    change

--- Homework.py
+++ Homework.py
@@ -2,12 +2,9 @@ pi = 3.14
 r = 5
 print('volume=',(4/3)*pi*r**3)
 
-
-
 price=24.95
 discount= 0.4*price
 quanity= 60
 shipping = 3+((quantity-1)*0.75)
 
-print('wholesale cost=', ((price-discount)*quantity+shipping)
-
+print('wholesale cost=', (price-discount)*quantity+shipping)
