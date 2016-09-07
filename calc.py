976c291acc3760b1724c448d36d6e36a37f432e7
rlauture1 <rlauture1@babson.edu>
2016-09-07 13:07:19 -0400

    homework complete

--- calc.py
+++ calc.py
@@ -7,3 +7,28 @@ print(10/1.61)#q2
 print(2565/6.211)#q3
 print(412.97/60)#q4
 print(60/6.88)#q5
+
+print('Exercise 1')
+print('Question #1')
+pi=3.14
+r=5
+print('volume=', (4/3)*pi*r**3)
+
+print('Question #2')
+price=24.95
+discount= 0.4*price
+quantity= 60
+shipping= 3+((quantity-1)*0.75)
+
+print('wholesale cost =', (price-discount)*quantity+shipping)
+print('Question #3')
+easy_time= 2*8.25
+tempo_time= 3*7.2
+total= easy_time + tempo_time
+end_minutes = total - 8
+print('7:%d' % (end_minutes))
+print('Question #4')
+percent= ((89-92)/82)*100
+print('%.1f%%' % percent)
+
+#collaborated with Theresa, Duncan, Hunter, and Wendy
