cffbc07cbed497f958c02688a3ee95a103d2d718
rlauture1 <rlauture1@babson.edu>
2016-09-13 22:00:32 -0400

    Quadratuic homework

--- lyrics.py
+++ lyrics.py
@@ -1,7 +1,18 @@
-print_lyrics():
-print('They hate but the broke though')
-print('when it is time to pop they a no show')
-
+def print_lyrics():  
+    print('They hate but the broke though.')
+    print('When it is time to pop they a no show')
 
 type(print_lyrics)
-print_lyrics()
+def repeat_lyrics():
+    print_lyrics()
+    print('ooouuuu-ooouuu')
+    print_lyrics()
+
+
+def give_me_a_break():
+    str1='break'
+    print('another break')
+    return str1
+
+
+
